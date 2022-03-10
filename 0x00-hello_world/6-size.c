e5ee04b9489cce9130258d793a1216e3318d48a3
iamtimbun <timothytimbun@gmail.com>
2022-03-10 10:13:29 -0800

    code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,16 @@
 int main(){
 int intType;
 float floatType;
-double doubleType;
+long longType;
 char charType;
+long long longType;
 
 // sizeof evaluates the size of a variable
 // printf("Size of int: %zu bytes\n", sizeof(intType));
 // printf("Size of float: %zu bytes\n", sizeof(floatType));
-// printf("Size of double: %zu bytes\n", sizeof(doubleType));
+// printf("Size of long: %zu bytes\n", sizeof(longType));
 // printf("Size of char: %zu byte\n", sizeof(charType));
+// printf("Size of long long: %zu byte\n", size of(long long));
 
 return 0;
 }
