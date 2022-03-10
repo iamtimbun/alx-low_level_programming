0a0c7f3b5e10237175fc9e47e1254e2a7d17bc51
iamtimbun <timothytimbun@gmail.com>
2022-03-10 07:33:48 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/*first program */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n);
+	puts("\"Programming is like building a multilingual puzzle"\n);
 	return(0);
 }	
