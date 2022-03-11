3a380cac802fcaa6ba93e782aa9ec9f349062b82
iamtimbun <timothytimbun@gmail.com>
2022-03-11 08:51:51 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+	putchar(ch);
+	for (ch='a'; ch<='z'; ch++)
+		putchar('\n');
+	getch();
+	Return(0);
+}
