713b6abd20c5affc0ba703c684b43c3efd2c7086
iamtimbun <timothytimbun@gmail.com>
2022-03-11 12:11:40 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int bi = 'z';
+
+	while (bi <= 'z')
+	{
+		putchar(bi);
+		bi--i;
+	}
+	putchar('\n');
+	return (0);
+}
