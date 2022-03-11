05915739433c3a5e4d69f82556eac6d7d2857ea2
iamtimbun <timothytimbun@gmail.com>
2022-03-11 12:27:18 -0800

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int nu = 0;
+	int bi = 'a';
+	
+	while (nu <= 9)
+	{
+		putchar( nu % 10 + '0');
+		nu++;
+	}
+	while (bi <= 'f')
+	{
+		putchar(bi);
+		bi++;
+	}
+	putchar('\n');
+	return  (0);
+}
