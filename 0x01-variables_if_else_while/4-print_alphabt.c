5626a2e85f1d688d847661b4d7e77fa1b89bfdb2
iamtimbun <timothytimbun@gmail.com>
2022-03-11 11:41:34 -0800

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 	int ch = 'a';
+
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
 		{
 			putchar (ch);
-			ch++;
 		}
-	}	
+		ch++;
+	}
 	putchar ('\n');
 	return (0);
 }
