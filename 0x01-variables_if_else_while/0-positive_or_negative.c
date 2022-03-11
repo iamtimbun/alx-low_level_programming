6bd0c1d9211fdbb11f84e9a68991b8060b2cc2ec
iamtimbun <timothytimbun@gmail.com>
2022-03-11 06:03:42 -0800

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0){
+	if(n > 0){
 		printf("is postive\n");
-		} else if(n=0){
+		} else if(n == 0){
 			printf("is zero\n");
-		} else if(n<0){
+		} else if(n < 0){
 			printf("is negative\n");
 	return(0);
 }
