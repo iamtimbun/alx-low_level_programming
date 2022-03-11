5f05ed9ca82b10738b3986b0d8692e3902ad91d9
iamtimbun <timothytimbun@gmail.com>
2022-03-11 09:51:03 -0800

    alphabb

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch = 'a'; int CH = 'A';
+	while(ch <= 'z')
+	{ 
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	while(CH <='Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	return (0);
+}
