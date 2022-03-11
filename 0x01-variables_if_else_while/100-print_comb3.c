e526b17409af95dafdbad410096ee080c6f212dd
iamtimbun <timothytimbun@gmail.com>
2022-03-11 15:06:37 -0800

    two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch=0;
+	int gh=0;
+
+	for(ch<=0)
+	{
+		putchar(ch);
+		ch++;
+		if()
+
+	}
+	return(0);
+}
+
