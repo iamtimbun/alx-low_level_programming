96bae3ba43b3db34ad9cfff634136a6d8ed60f3f
iamtimbun <timothytimbun@gmail.com>
2022-03-13 10:49:42 -0700

    invent

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int l;
 	int m;
 
-	for ( l=0; l<=9; l++)
+	for (l = 0 ; l <= 9; l++)
 	{
-		for ( m=0; m<=9; m++)
+		for (m = 0; m <= 9; m++)
 		{
-			if ( l<m )
+			if (l < m)
 			{
-				putchar (( l % 10 ) + '0');
-				putchar (( m % 10) + '0');
-				if (l!=8)
+				putchar ((l % 10) + '0');
+				putchar ((m % 10) + '0');
+				if (l != 8)
 				{
 					putchar (',');
 					putchar (' ');
