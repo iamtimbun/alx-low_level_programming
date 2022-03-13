fd5a90888f4b2dff64713cee068ee70b7a12f0aa
iamtimbun <timothytimbun@gmail.com>
2022-03-13 10:40:44 -0700

    invent

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int l;
+	int m;
+
+	for ( l=0; l<=9; l++)
+	{
+		for ( m=0; m<=9; m++)
+		{
+			if ( l<m )
+			{
+				putchar (( l % 10 ) + '0');
+				putchar (( m % 10) + '0');
+				if (l!=8)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
