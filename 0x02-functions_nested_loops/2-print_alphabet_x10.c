ff16bbaebf1feaffdc72f3890e78278bcbb88904
iamtimbun <timothytimbun@gmail.com>
2022-03-15 15:40:35 +0100

    Alphabet10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *main - check the code
+  *
+  *Return: Always 0.
+  */
+void print_alphabet_x10(void);
+{
+	int bh;
+	while (bh = 'a'; bh <= 'z'; bh++);
+	_putchar (bh);
+	_putchar ('\n');
+	return (0);
+}
