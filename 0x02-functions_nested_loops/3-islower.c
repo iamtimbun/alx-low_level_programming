259e51a281886afd55107aa1d92b32c0b7bd90db
iamtimbun <timothytimbun@gmail.com>
2022-03-16 00:04:01 +0100

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+  *int _islower - check for small case
+  *
+  *Return: Always 0.
+  */
+
+int _islower(int c)
+{
+	char c;
+	if(_islower(c))
+	{
+		return (1);
+	}
