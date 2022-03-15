393654b7b8770a7426330d614d66208d91d0738e
iamtimbun <timothytimbun@gmail.com>
2022-03-16 00:46:14 +0100

    low

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "main.h"
+#include<stdlib.h>
 
 /**
   *_islower - check for small case
-  *
+  *@c: parameter given
   *Return: Always 0.
   */
 
-int _islower(int c);
-int main (void)
+int _islower(int c)
 {
-	int c;
-
-	if(_islower(c) == 0)
-	{
-		_putchar(1);
-	}else{
-		_putchar(0);
-	}
-	return 0;
+	return (c >= 'a' && c <=  'z');
 }
