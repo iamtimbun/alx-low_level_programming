8609a5796cd6b7ebfcace2160f2b3772d5480878
iamtimbun <timothytimbun@gmail.com>
2022-03-16 21:59:21 +0100

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  *print_last_digit - print last digit
+  *
+  *@int: integer parameter
+  *@n: parameter
+  */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return 0;
+}
