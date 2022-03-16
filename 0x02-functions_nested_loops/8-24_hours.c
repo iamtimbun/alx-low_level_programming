b37c85ce2ba176f6a00a66188a4c5d61b5f4b466
iamtimbun <timothytimbun@gmail.com>
2022-03-16 22:56:50 +0100

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  *jack_bauer(void) - print numbers
+  *
+  *return: void
+  */
+
+void jack_bauer(void)
+{
+	int n;
+	if (n <= 24)
+	{
+		_putchar (n);
+		n++;
+		_putchar ('\n');
+	}
+}				
