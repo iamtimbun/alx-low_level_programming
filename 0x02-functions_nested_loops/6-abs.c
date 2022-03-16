22ecc60ae800a227fe9edc9dddb63cb1899cff66
iamtimbun <timothytimbun@gmail.com>
2022-03-16 21:35:31 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *_abs- checkthe integer
+  *Return: Whatever
+  *@n: parameter
+  */
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+}
