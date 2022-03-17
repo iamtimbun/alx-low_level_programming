0d65215deba0fd87961ccf91f603a44036a457eb
iamtimbun <timothytimbun@gmail.com>
2022-03-17 20:37:01 +0100

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *print_numbers - print numbers
+  *Return: 0
+  */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a <= 9; a++)
+	{
+		_putchar (a);
+	}
+	_putchar ('\n');
+}
