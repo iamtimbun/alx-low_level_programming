9914da1356397ebc53bc122e34ba251511115967
iamtimbun <timothytimbun@gmail.com>
2022-03-17 19:44:38 +0100

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *_isupper(int c); - uppercase
+  *
+  *Return: various things
+  */
+
+int _isupper(int c)
+{
+	if(c>101)
+	{
+	return (1);
+	}else
+	{return (0);
+	}
+}
