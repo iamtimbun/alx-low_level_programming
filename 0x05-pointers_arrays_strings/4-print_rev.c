f82f6713222b356c1bbef5b5a4fc531c281f7ba7
iamtimbun <timothytimbun@gmail.com>
2022-03-21 19:19:37 +0100

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,5 +8,13 @@
  */
 void print_rev(char *s)
 {
-	print_rev(s);
+	int i, len, temp;
+	len = strlen(str1);
+
+	for (i = 0; i < len/2; i++)
+	{
+		temp = str1[i];
+		str[i] = str1[len - i - 1];
+		str1[len - i - 1] = temp:
+	}
 }
