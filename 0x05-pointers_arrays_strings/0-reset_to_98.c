1fd14264024332b5c1d3efe237c6fc1164d80656
iamtimbun <timothytimbun@gmail.com>
2022-03-21 16:16:48 +0100

    battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *void reset_to_98 - Reset to points
+  *
+  *Return: 0
+  */
+
+void reset_to_98(int *n)
+{
+	int p = 98;
+	*n = p;
+}
