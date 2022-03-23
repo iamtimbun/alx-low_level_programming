57b2458470f00833ed543765cbc5275167d677ea
iamtimbun <timothytimbun@gmail.com>
2022-03-23 22:27:35 +0000

    comp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,19 @@
 
 /**
  *_strcmp-compare two strings
+ *@s1: parameter
+ *@s2: parameter
  *Return: 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
+	int i = 0;
+	while (s1[i] == s2[i])
 	{
-		return (0);
-	}else if (s1 < s2)
-	{
-		return (-1);
-	}else if (s1 > s2)
-	{
-		return (1);
-	}
+		if(s1[i] == '\0' && s2[i] == '\0')
+		break;
+	i++;
+}
+	return s1[i] - s2[i];
 }
