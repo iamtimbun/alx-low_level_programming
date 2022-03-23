47451996884b53937105897471a1c9f991205b14
iamtimbun <timothytimbun@gmail.com>
2022-03-23 20:45:32 +0000

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @_strncpy- String copy
+ * @n: integer
+ * @Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return dest;
+}
