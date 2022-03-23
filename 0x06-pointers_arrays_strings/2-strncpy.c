fa2b5cbf05da381de02e0192b117e303cc5787e7
iamtimbun <timothytimbun@gmail.com>
2022-03-23 20:53:44 +0000

    string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,11 @@
 #include "main.h"
 
 /**
- * _strncpy: String copy
+ * _strncpy- String copy
  * @n: integer
- * @Return: 0
+ * Return: 0
+ * @dest: destination
+ * @src: source
  */
 
 char *_strncpy(char *dest, char *src, int n)
