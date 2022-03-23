544a0b8bd4bc712b53c0942e6514c45811cb7432
iamtimbun <timothytimbun@gmail.com>
2022-03-23 08:50:53 +0000

    More arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * char *_strcat- Join two strings
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return dest;
+}
