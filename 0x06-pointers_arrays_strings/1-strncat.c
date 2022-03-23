dd3f6d018ba0dd50e8f6470f7030ce3510488188
iamtimbun <timothytimbun@gmail.com>
2022-03-23 19:43:22 +0000

    cat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,18 +3,20 @@
 #include <string.h>
 
 /**
- * char *_strncat- Concatenate two strings
+ * _strncat- Concatenate two strings
  * @n: parameter
+ * @dest: destination
+ * @src: source
+ * Return: 0
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = strlen(dest);
-               int i;
+	int i;
 
-               for (i = 0 ; i < n && src[i] != '\0' ; i++)
-                   dest[dest_len + i] = src[i];
-               dest[dest_len + i] = '\0';
-
-               return dest;
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
