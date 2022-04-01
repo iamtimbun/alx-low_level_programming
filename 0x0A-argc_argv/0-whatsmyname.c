f7649c3497d051285eab4749db590e26b95d3d95
iamtimbun <timothytimbun@gmail.com>
2022-04-01 22:00:54 +0000

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main(int argc, char *argv[])- main function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf ("argv[%d] = %s\n", i, argv[i]);
+		printf("argv[%d] = %s\n", i, argv[i]);
 	}
 	return (0);
 }
