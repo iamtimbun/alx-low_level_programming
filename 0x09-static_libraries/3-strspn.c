71179a4929eea67df9d0c17729957ed8d56d526d
iamtimbun <timothytimbun@gmail.com>
2022-04-02 01:22:29 +0000

    life

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,1 +1,14 @@
-unsigned int _strspn(char *s, char *accept);
+#include "main.h"
+#include <string.h>
+
+/**
+*_strspn - function for length of prefix
+*@s: string
+*@accept: prefix
+*Return: number of bytes
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
