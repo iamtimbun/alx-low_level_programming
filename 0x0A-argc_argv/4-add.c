574a9e2717e2bcf44888cac3a6cb5388c8dbd3f1
iamtimbun <timothytimbun@gmail.com>
2022-04-05 21:16:42 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,51 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main(int argc, char *argv[])- main function
- * @argc: argument
- * @argv: argument
+ *checker - checks for numbers
+ *@s: string
+ *Return: 1
+ */
+
+int checker(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * main - prints
+ * @argc: arguments number
+ * @argv: arguments entered
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
-	int zero = 0;
+	int i, j, k;
 
 	for (i = 1; i < argc; i++)
 	{
-		sum += atoi(argv[i]);
-		if (i < 0)
+		if (checker(argv[i]) == 0)
 		{
-			printf("%d\n", zero);
+			printf("Error\n");
 			return (1);
 		}
+		j = atoi(argv[i]);
+		if (j >= 0)
+		{
+			k = j + k;
+		}
 	}
-	printf("%d\n", sum);
+	printf("%d\n", k);
 	return (0);
 }
