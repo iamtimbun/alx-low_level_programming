d05a757d00708f9fcfc8da40d0168a8b595716d6
iamtimbun <timothytimbun@gmail.com>
2022-04-11 21:15:26 +0000

    absolute

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main(int argc, char *argv[])- main function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char ** argv)
+{
+    printf("%s", argv[0]);
+    return 0;
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#include <stdio.h>
+#include <stdlib.h>
+#define ABS(x) abs(x)
+#endif
