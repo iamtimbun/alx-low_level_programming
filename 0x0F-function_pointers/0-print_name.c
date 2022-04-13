b729aa9e0cb5fd0b695b357c240bdcdfea9d5917
iamtimbun <timothytimbun@gmail.com>
2022-04-13 21:10:22 +0000

    print

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name(char *name, void (*f)(char *)) - print a name
- * @name - name of the person
- *
+ * print_name - print a name
+ * @name : name of the person
+ * @f: pointer
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
-{	
+{
 	if (name == NULL || f == NULL)
 	{
 		return;
